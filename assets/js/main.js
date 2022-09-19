885b2246c8ef5834835054ac27431c11c99b64f7
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2022-09-19 15:20:11 +0530

    contact form js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,16 @@
 function erralt() {
     alert("oops! Something went wrong.");
 }
+
+// Confirmation Alert (contact.html)
+function confAlert(){
+    if (confirm("Are you sure? You want to send this!") == true){
+      if (document.getElementById("bug").checked == true){
+        alert("Thank You! for reporting a bug.");
+      } else{
+        alert("Thank You! for your valuable Feedback!");
+      }
+    } else{
+      alert("Looks like you Cancelled!");
+    }
+  }
